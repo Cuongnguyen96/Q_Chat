f58077c43bc8f9056fcdf17c3c789e9ab1d55793
Cuongnguyen96 <cuongnguyen96td@gmail.com>
2019-07-29 19:34:41 +0700

    [1.0.0] register user 1.

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation  = {
+    email_incorect: "Email Not Recongintion",
+    gender_incorect: "Male Or Female",
+    passwork_incorect: "Regulation Character or Number",
+    passwork_confirmation_incorect: "Incorrect",
+};
