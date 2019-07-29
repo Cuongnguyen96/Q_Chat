df1f3bfb6a30717b2a546413f87d888564667fae
Cuongnguyen96 <cuongnguyen96td@gmail.com>
2019-07-29 19:34:41 +0700

    [1.0.0] register user 1.

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,6 +2,7 @@ import express from "express";
 //import authController from "./../controllers/authController";
 //import homeController from "./../controllers/homeController";
 import {home, auth} from "./../controllers/index";
+import {authValid} from "./../validation/index";
 
 let router = express.Router();
 
@@ -15,7 +16,8 @@ let initRoutes = (app) => {
     router.get("/",  home.getHome);
     
     router.get("/login-register",  auth.getloginRegister);
-    router.get("/logout",  auth.getLogout);
+    //router.get("/logout",  auth.getLogout);
+    router.post("/register", authValid.register, auth.postRegister);
 
     return app.use("/", router);
 }
